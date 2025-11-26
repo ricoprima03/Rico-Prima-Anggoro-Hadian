9731d8c541f9d90999e0cac0e9786d6388ee0934
ricoprima03 <ricoprima03@gmail.com>
2025-11-26 16:22:29 +0700

    HALOO

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,12 @@
-
+<DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Rendangmu - Lazismu</title>
+	<link rel="icon" type="image/png" href="/logo-lazismu.png">
+</head>
+<body style="margin: 0; padding: 0; background: #f9f9f9;">
 <!-- Import Jakarta Sans font -->
 <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;700&display=swap" rel="stylesheet">
 
