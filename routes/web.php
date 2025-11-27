43f45008e9b786773f81115033c665f344ad7d93
ricoprima03 <ricoprima03@gmail.com>
2025-11-28 00:16:14 +0700

    rev route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
-
 <?php
-Route::get('/Pembayaran', function () {
-    return view('layouts.Pembayaran');
-});
-
 use Illuminate\Support\Facades\Route;
 
 Route::view('/qurban', 'layouts.qurban')->name('qurban');
 Route::get('/', function () {
     return view('layouts.home');
+});
+
+Route::get('/Pembayaran', function () {
+    return view('layouts.Pembayaran');
 });
